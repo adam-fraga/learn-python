0172406763fa490bfe3fdcffcf732c3aab561941
Adam Fraga <adam.fraga@laplateforme.io>
2021-10-02 14:02:19 +0200

    comment & delete mess i left debug messg as memo

--- jour04/job03/recursive_pow.py
+++ jour04/job03/recursive_pow.py
@@ -1,24 +1,17 @@
 nb = int(input("Entre un votre nombre"))
 
-# Créer un programme demandant à l’utilisateur de renseigner un nombre
-# entier. Votre programme devra calculer x^n, ou n est le nombre fourni par
-# l’utilisateur, sans utiliser de fonction autre que les vôtres. Attention, vous ne
-# devez utiliser ni while, ni for, ni foreach ni ... boucle. Seulement de la
-# récursivité.
-
-
 def ft_pow_recursive(nb):
     if nb == 0:  # Condition d'arrêt fixé si input user = 0
         print("test2")
         return True
     else:
-        print("Avant")
-        ft_pow_recursive(nb - 1)  # Décrémeente l'entré utilisateur
+        print("Avant") #(Ordre d'empilage FILO)
+        ft_pow_recursive(nb - 1)  # Décrémeente l'entrée utilisateur
         print("Après")
         nb = pow(
             nb, nb
-        )  # LIFO  La dernière valeur a être popé étant la même que l'entré utilisateur pow -> elle même
+        )  #ordre dépilage LIFO  La dernière valeur a être popé sur la stack
+           #est la première a être dépile donc élève celle ci puissance elle même
         print("Pow:{}".format(nb))
-
 
 ft_pow_recursive(nb)
