e4f448c1ec6f71400ba8d053d7211f96cf3660e1
Adam Fraga <adam.fraga@laplateforme.io>
2021-10-02 22:54:07 +0200

    Test approche par attaque de piece avant placement

--- jour04/job08/ladys_first.py
+++ jour04/job08/ladys_first.py
@@ -1,17 +1,20 @@
-from os import error
 import numpy as np
+from numpy.core.records import array
 
 
-class Game:
+class Game(object):
     """Représentation du jeu de dames."""
 
-    def __init__(self):
-        self._size = 0
-        self._board = np.zeros([self._size], [self._size])
+    # Constructeur
+    def __init__(self, size):
+        self._size = size
+        self._board = np.empty((self._size, self._size))
         # Syntaxe de compréhension de listes voir mes notes (code doc Python)
         self._axe_X = [x for x in range(self._size)]
         self._axe_Y = [y for y in range(self._size)]
         self._dames = self._size
+
+    # Getter & setters
 
     # Récupère la taille global
     def _getsize(self):
@@ -23,12 +26,15 @@ class Game:
 
     # Initialise la taille du plateau
     def _setsize(self, size):
-        if int(size) > 2:
+
+        if int(size) > 2 and isinstance(size, int):
             try:
                 self._size = size
             except Exception as e:
                 print("Le plateau doit avoir une taille superieur a 2\n")
                 raise e
+        else:
+            print("Les dimensions du jeu doivent être de 3 minimum")
 
     # Récupère le plateau du jeu (a afficher avec un print ou faire methode)
     def _getboard(self):
@@ -40,11 +46,14 @@ class Game:
 
     # Initialise le plateau du joueur avec un tableau numpy n*n
     def _setboard(self, board):
-        try:
-            self._board = board
-        except Exception as e:
-            print("Impossible de réinitialiser le plateau de jeu \n")
-            raise e
+        if isinstance(board, list):
+            try:
+                self._board = board
+            except Exception as e:
+                print("Impossible de réinitialiser le plateau de jeu \n")
+                raise e
+        else:
+            print("Le plateau de jeu doit être un tableau")
 
     # Récupère l'axe des abscisses (sous forme de liste)
     def _getaxe_X(self):
@@ -56,34 +65,42 @@ class Game:
 
     # initialise la liste des abscisses
     def _setaxe_X(self, axe_X):
-        try:
-            self._axe_X = axe_X
-        except Exception as e:
-            print("Impossible d'initialiser l'axe X \n")
-            raise e
+        if isinstance(axe_X, list):
+            try:
+                self._axe_X = axe_X
+            except Exception as e:
+                print("Impossible d'initialiser l'axe X \n")
+                raise e
+        else:
+            ("L'axe X doit être une liste de valeur")
 
     # Récupère l'axe des ordinnées (sous forme de liste)
     def _getaxe_Y(self):
         try:
-            return _setaxe_Y
+            return self._axe_Y
         except Exception as e:
             print("Impossible de récupéré l'axe Y \n")
             raise e
 
     # initialise la liste des ordonnées
     def _setaxe_Y(self, axe_Y):
-        try:
-            self._axe_Y = axe_Y
-        except Exception as e:
-            print("Impossible d'initialiser l'axe Y \n")
-            raise e
+        if isinstance(axe_Y, list):
+            try:
+                self._axe_Y = axe_Y
+            except Exception as e:
+                print("Impossible d'initialiser l'axe Y \n")
+                raise e
+        else:
+            print("L'axe des Y doit être une liste")
 
     # Récupère le nombre de dame = a la taille de l'input user
+    # Les dames sont une valeur constante egal à l'input user
+    # La constante n'existant pas en python pas de setter
     def _getdames(self):
         try:
             return self._dames
         except Exception as e:
-            print("Impossible de réxupérè le nombre de dames \n")
+            print("Impossible de récupéré le nombre de dames \n")
             raise e
 
     # Respect de l'ordre des parametres Voir Notes code doc Python property
@@ -91,4 +108,32 @@ class Game:
     board = property(_getboard, _setboard)
     axe_X = property(_getaxe_X, _setaxe_X)
     axe_Y = property(_getaxe_Y, _setaxe_Y)
-    dames = property(_getdames, _setdames)
+    dames = property(_getdames)
+
+    # Essaie de mouvement attaque allée et retour sur plateau par récursion
+    def dame_attack(self, i):
+        if i == 0:
+            return True
+        else:
+            print(i)
+            self.dame_attack(i - 1)
+            print(i)
+
+
+size = input("Entrez la taille du jeu\n")
+size = int(size)
+if size:
+    print("\nDébut de la partie\n")
+    print("Initialisation du tableau...\n")
+    # Initialise le jeu
+    Game1 = Game(size)
+    print(f"Votre plateau fait {Game1._getsize()} * {Game1._getsize()} case\n")
+    print(f"Nombre de dames: {Game1._getdames()} représenté par des X \n")
+    print("Les cases vides sont représenté par la lettre O\n")
+    # Avant de placer dame programmer ses attaques sur plateau en vu de définir les cases disponible?
+
+
+else:
+    print(
+        "Vous devez entrer une valeur entière qui représentera la taille du plateau de jeu"
+    )
